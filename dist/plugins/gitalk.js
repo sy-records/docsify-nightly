75fcdfb7c6837bc69256e9b5a0956432bcd42493
sy-records <sy-records@users.noreply.github.com>
2024-12-11 03:47:14 +0000

    Nightly release for docsify

--- dist/plugins/gitalk.js
+++ dist/plugins/gitalk.js
@@ -6,23 +6,36 @@
  */
 (function() {
     "use strict";
-    function install(hook) {
-        const dom = Docsify.dom;
-        hook.mounted((_ => {
-            const div = dom.create("div");
-            div.id = "gitalk-container";
-            const main = dom.getNode("#main");
-            div.style = `width: ${main.clientWidth}px; margin: 0 auto 20px;`;
-            dom.appendTo(dom.find(".content"), div);
-        }));
-        hook.doneEach((_ => {
-            const el = document.getElementById("gitalk-container");
-            while (el.hasChildNodes()) {
-                el.removeChild(el.firstChild);
-            }
-            gitalk.render("gitalk-container");
-        }));
+    function getDefaultExportFromCjs(x) {
+        return x && x.__esModule && Object.prototype.hasOwnProperty.call(x, "default") ? x["default"] : x;
     }
-    window.$docsify = window.$docsify || {};
-    $docsify.plugins = [ install, ...$docsify.plugins || [] ];
+    var gitalk$2 = {};
+    var hasRequiredGitalk;
+    function requireGitalk() {
+        if (hasRequiredGitalk) return gitalk$2;
+        hasRequiredGitalk = 1;
+        function install(hook) {
+            const dom = Docsify.dom;
+            hook.mounted((_ => {
+                const div = dom.create("div");
+                div.id = "gitalk-container";
+                const main = dom.getNode("#main");
+                div.style = `width: ${main.clientWidth}px; margin: 0 auto 20px;`;
+                dom.appendTo(dom.find(".content"), div);
+            }));
+            hook.doneEach((_ => {
+                const el = document.getElementById("gitalk-container");
+                while (el.hasChildNodes()) {
+                    el.removeChild(el.firstChild);
+                }
+                gitalk.render("gitalk-container");
+            }));
+        }
+        window.$docsify = window.$docsify || {};
+        $docsify.plugins = [ install, ...$docsify.plugins || [] ];
+        return gitalk$2;
+    }
+    var gitalkExports = requireGitalk();
+    var gitalk$1 = getDefaultExportFromCjs(gitalkExports);
+    return gitalk$1;
 })();
--- dist/plugins/gtag.js
+++ dist/plugins/gtag.js
@@ -6,53 +6,66 @@
  */
 (function() {
     "use strict";
-    function appendScript(id) {
-        const script = document.createElement("script");
-        script.async = true;
-        script.src = "https://www.googletagmanager.com/gtag/js?id=" + id;
-        document.body.appendChild(script);
+    function getDefaultExportFromCjs(x) {
+        return x && x.__esModule && Object.prototype.hasOwnProperty.call(x, "default") ? x["default"] : x;
     }
-    function initGlobalSiteTag(id) {
-        appendScript(id);
-        window.dataLayer = window.dataLayer || [];
-        window.gtag = window.gtag || function() {
-            window.dataLayer.push(arguments);
+    var gtag$1 = {};
+    var hasRequiredGtag;
+    function requireGtag() {
+        if (hasRequiredGtag) return gtag$1;
+        hasRequiredGtag = 1;
+        function appendScript(id) {
+            const script = document.createElement("script");
+            script.async = true;
+            script.src = "https://www.googletagmanager.com/gtag/js?id=" + id;
+            document.body.appendChild(script);
+        }
+        function initGlobalSiteTag(id) {
+            appendScript(id);
+            window.dataLayer = window.dataLayer || [];
+            window.gtag = window.gtag || function() {
+                window.dataLayer.push(arguments);
+            };
+            window.gtag("js", new Date);
+            window.gtag("config", id);
+        }
+        function initAdditionalTag(id) {
+            window.gtag("config", id);
+        }
+        function init(ids) {
+            if (Array.isArray(ids)) {
+                initGlobalSiteTag(ids[0]);
+                ids.forEach(((id, index) => {
+                    if (index > 0) {
+                        initAdditionalTag(id);
+                    }
+                }));
+            } else {
+                initGlobalSiteTag(ids);
+            }
+        }
+        function collect() {
+            if (!window.gtag) {
+                init($docsify.gtag);
+            }
+            window.gtag("event", "page_view", {
+                page_title: document.title,
+                page_location: location.href,
+                page_path: location.pathname
+            });
+        }
+        const install = function(hook) {
+            if (!$docsify.gtag) {
+                console.error("[Docsify] gtag is required.");
+                return;
+            }
+            hook.beforeEach(collect);
         };
-        window.gtag("js", new Date);
-        window.gtag("config", id);
+        window.$docsify = window.$docsify || {};
+        $docsify.plugins = [ install, ...$docsify.plugins || [] ];
+        return gtag$1;
     }
-    function initAdditionalTag(id) {
-        window.gtag("config", id);
-    }
-    function init(ids) {
-        if (Array.isArray(ids)) {
-            initGlobalSiteTag(ids[0]);
-            ids.forEach(((id, index) => {
-                if (index > 0) {
-                    initAdditionalTag(id);
-                }
-            }));
-        } else {
-            initGlobalSiteTag(ids);
-        }
-    }
-    function collect() {
-        if (!window.gtag) {
-            init($docsify.gtag);
-        }
-        window.gtag("event", "page_view", {
-            page_title: document.title,
-            page_location: location.href,
-            page_path: location.pathname
-        });
-    }
-    const install = function(hook) {
-        if (!$docsify.gtag) {
-            console.error("[Docsify] gtag is required.");
-            return;
-        }
-        hook.beforeEach(collect);
-    };
-    window.$docsify = window.$docsify || {};
-    $docsify.plugins = [ install, ...$docsify.plugins || [] ];
+    var gtagExports = requireGtag();
+    var gtag = getDefaultExportFromCjs(gtagExports);
+    return gtag;
 })();
