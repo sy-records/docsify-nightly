00752c219098e43260ed49d3e7f201a5f381d16d
sy-records <sy-records@users.noreply.github.com>
2025-03-11 03:45:09 +0000

    Nightly release for docsify

--- dist/docsify.js
+++ dist/docsify.js
@@ -4825,7 +4825,7 @@
                             if (typeof document === "undefined") {
                                 return null;
                             }
-                            if ("currentScript" in document && 1 < 2) {
+                            if (document.currentScript && document.currentScript.tagName === "SCRIPT" && 1 < 2) {
                                 return document.currentScript;
                             }
                             try {
